6f8e84870436801ee5e868a845b8c2e16ba3aec8
Mickaël CHAVE <mchave3@live.fr>
2026-01-26 15:24:45 +0100

    Fix path separators in UpdateWindowsEsdCatalogCache.build.ps1 for consistency and revert runner environment to windows-latest in update workflow

--- .build/UpdateWindowsEsdCatalogCache.build.ps1
+++ .build/UpdateWindowsEsdCatalogCache.build.ps1
@@ -1,14 +1,14 @@
 task Update_WindowsEsdCatalogCache {
     $repoRoot = (Get-Location).Path
 
-    $fn0 = Join-Path -Path $repoRoot -ChildPath 'source/Private/Core/IO/Invoke-MCDDownload.ps1'
-    $fn1 = Join-Path -Path $repoRoot -ChildPath 'source/Private/OperatingSystem/ConvertFrom-MCDProductsXml.ps1'
-    $fn2 = Join-Path -Path $repoRoot -ChildPath 'source/Private/OperatingSystem/Update-MCDWindowsEsdCatalogCache.ps1'
+    $fn0 = Join-Path -Path $repoRoot -ChildPath 'source\Private\Core\IO\Invoke-MCDDownload.ps1'
+    $fn1 = Join-Path -Path $repoRoot -ChildPath 'source\Private\OperatingSystem\ConvertFrom-MCDProductsXml.ps1'
+    $fn2 = Join-Path -Path $repoRoot -ChildPath 'source\Private\OperatingSystem\Update-MCDWindowsEsdCatalogCache.ps1'
 
     . $fn0
     . $fn1
     . $fn2
 
-    $outDir = Join-Path -Path $repoRoot -ChildPath 'source/Cache/Operating System'
+    $outDir = Join-Path -Path $repoRoot -ChildPath 'source\Cache\Operating System'
     Update-MCDWindowsEsdCatalogCache -OutputDirectory $outDir
 }
